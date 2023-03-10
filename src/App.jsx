7dc4f94b6d65c38c4f181c3f095813013a601b8e
Diego Vistoso <diegoivg98@gmail.com>
2023-03-10 14:51:54 -0300

    agregar Provider y TaskList a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import { TaskProvider } from "./components/TaskContext";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
     <div className="App">
-     
+      <TaskProvider>
+        <TaskList />
+      </TaskProvider>
     </div>
   );
 }
